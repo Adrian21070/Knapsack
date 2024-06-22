681864d986fc7914cba89b65834e8ab8026f6e92
Adrian M <adrian.im0502@gmail.com>
2024-06-22 00:27:15 -0600

    A new button solve was added. Solve triggers a signal to start selecting items by an algorithm. The selection is the one that triggers the graphs.

--- Visualization/dashboard.py
+++ Visualization/dashboard.py
@@ -3,7 +3,8 @@ Code to visualize the algorithm solving a knapsack instance
 """
 
 # Import Libraries
-from dash import Dash, dcc, html, Input, Output, callback
+from dash import Dash, dcc, html, Input, State, Output, callback, ctx
+from dash.exceptions import PreventUpdate
 
 
 # Initialize app
@@ -29,7 +30,15 @@ app.layout = html.Div(
         # Select Instance to solve
         html.Div([
             html.H3("Select instance to solve:"),
-            dcc.Dropdown(["Instance-1", "Instance-2"], "Instance-1", id="instance-selector")
+            html.Div([
+                dcc.Dropdown(["Instance-1", "Instance-2"], "Instance-1", id="instance-selector")
+            ], style={"width": "70%", "height": "25px", "display": "inline-block",
+                      "margin-right": "0px", "margin-left": "0px"}),
+            html.Div([
+                html.Button("Solve", id="solve-instance", n_clicks=0, style={"fontSize":"1.25em"}),
+            ], style={"width": "30%", "height": "25px", "display": "inline-block",
+                      "margin-left": "0px", "margin-right": "0px"})
+
         ], style={"width": "33%", "textAlign": "center", "margin-left": "auto", "margin-right": "auto"}),
 
         
@@ -61,14 +70,21 @@ app.layout = html.Div(
             ]
         ),
 
+        # Step intervals
+        dcc.Interval(id="counter-interval", interval=5000, n_intervals=0),
         
         # Memory section
-        dcc.Store(id="available-items-data"),
-        dcc.Store(id="knapsack-data"),
+        ## Initial data
+        dcc.Store(id="initial-items-data"),
+        dcc.Store(id="initial-knapsack-data"),
         dcc.Store(id="colors"),
         dcc.Store(id="weight-data"),
 
-
+        ## Current data
+        dcc.Store(id="available-items-data"),
+        dcc.Store(id="knapsack-data"),
+        dcc.Store(id="trigger-selection", data=False),
+        dcc.Store(id="trigger-graphs", data=False)
     ]
 )
 
@@ -77,8 +93,8 @@ app.layout = html.Div(
 # ------------------------------------
 
 @callback(
-    Output("available-items-data", "data"),
-    Output("knapsack-data", "data"),
+    Output("initial-items-data", "data"),
+    Output("initial-knapsack-data", "data"),
     Output("colors", "data"),
     Output("weight-data", "data"),
     Input("instance-selector", "value")
@@ -88,7 +104,7 @@ def initialize_instance(instance_name: str):
     print("Initializer was called")
 
     # Load instance
-    items = [(1, 10, 10), (2,25,80)]
+    items = [(1,10,10), (2,25,80)]
 
     # Empty knapsack
     knapsack = []
@@ -104,6 +120,47 @@ def initialize_instance(instance_name: str):
 
 
 # ------------------------------------
+# Initialize the interval
+# ------------------------------------
+@callback(
+    Output("counter-interval", "n_intervals"),
+    Output("trigger-selection", "data"),
+    Input("solve-instance", "n_clicks"),
+    prevent_initial_call=True
+)
+def reset_interval(n):
+    """
+    Initialize to 0 the interval, and set True to select_items
+    """
+    print("Interval was reset")
+
+    return 0, True
+
+
+# ------------------------------------
+# Select and item by an algorithm
+# ------------------------------------
+@callback(
+    Output("available-items-data", "data"),
+    Output("knapsack-data", "data"),
+    Output("trigger-graphs", "data"),
+    State("available-items-data", "data"),
+    State("knapsack-data", "data"),
+    Input("counter-interval", "n_intervals"),
+    Input("trigger-selection", "data"),
+    prevent_initial_call = True,
+)
+def select_item(items, knapsack, counter, trigger):
+    print("Selector was called")
+
+    if trigger:
+        print("RIGHT")
+        return items, knapsack, True
+
+    return items, knapsack, PreventUpdate
+
+
+# ------------------------------------
 # Functions to graph
 # ------------------------------------
 
@@ -112,12 +169,14 @@ Items graph
 """
 @callback(
     Output("available-items-graph", "figure"),
-    Input("available-items-data", "data")
+    State("available-items-data", "data"),
+    Input("trigger-graphs", "data"),
+    prevent_initial_call=True
 )
-def update_items_graph(items):
+def update_items_graph(items, trigger):
     # Create / Update the items graph
     print("Items graph was called")
-    pass
+
 
 
 """
@@ -125,13 +184,14 @@ Knapsack graph
 """
 @callback(
     Output("knapsack-graph", "figure"),
-    Input("knapsack-data", "data")
+    State("knapsack-data", "data"),
+    Input("trigger-graphs", "data"),
+    prevent_initial_call=True
 )
-def update_items_graph(items):
+def update_items_graph(items, trigger):
     # Create / Update the items graph
     print("Knapsack graph was called")
-    pass
-
+    
 
 
 if __name__ == "__main__":
