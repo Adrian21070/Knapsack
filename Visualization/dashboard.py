a2a11a4c88909d35e9bbf3118815656ebe4249be
Adrian M <adrian.im0502@gmail.com>
2024-06-21 22:20:55 -0600

    The instance selector now has a callback. After a selection, it loads the data and store it in memory. This actions creates a chain to make the knapsack and items graphs.

--- Visualization/dashboard.py
+++ Visualization/dashboard.py
@@ -3,7 +3,7 @@ Code to visualize the algorithm solving a knapsack instance
 """
 
 # Import Libraries
-from dash import Dash, dcc, html, Input, Output
+from dash import Dash, dcc, html, Input, Output, callback
 
 
 # Initialize app
@@ -29,7 +29,7 @@ app.layout = html.Div(
         # Select Instance to solve
         html.Div([
             html.H3("Select instance to solve:"),
-            dcc.Dropdown(["Instance-1", "Instance-2"], "Instance-1")
+            dcc.Dropdown(["Instance-1", "Instance-2"], "Instance-1", id="instance-selector")
         ], style={"width": "33%", "textAlign": "center", "margin-left": "auto", "margin-right": "auto"}),
 
         
@@ -59,9 +59,80 @@ app.layout = html.Div(
                 ], style={"width":"45%", "display": "inline-block", "padding": "0 20"})
 
             ]
-        )
+        ),
+
+        
+        # Memory section
+        dcc.Store(id="available-items-data"),
+        dcc.Store(id="knapsack-data"),
+        dcc.Store(id="colors"),
+        dcc.Store(id="weight-data"),
+
+
     ]
 )
+
+# ------------------------------------
+# Initialize variables of the instance
+# ------------------------------------
+
+@callback(
+    Output("available-items-data", "data"),
+    Output("knapsack-data", "data"),
+    Output("colors", "data"),
+    Output("weight-data", "data"),
+    Input("instance-selector", "value")
+)
+def initialize_instance(instance_name: str):
+    # After an instance is selected, load the instance and initialize variables
+    print("Initializer was called")
+
+    # Load instance
+    items = [(1, 10, 10), (2,25,80)]
+
+    # Empty knapsack
+    knapsack = []
+
+    # Create random colors for the item visualization
+    colors = []
+
+    # Create auxiliar variables
+    min_weight = min([item[1] for item in items])
+    max_weight = max([item[1] for item in items])
+
+    return items, knapsack, colors, [min_weight, max_weight]
+
+
+# ------------------------------------
+# Functions to graph
+# ------------------------------------
+
+"""
+Items graph
+"""
+@callback(
+    Output("available-items-graph", "figure"),
+    Input("available-items-data", "data")
+)
+def update_items_graph(items):
+    # Create / Update the items graph
+    print("Items graph was called")
+    pass
+
+
+"""
+Knapsack graph
+"""
+@callback(
+    Output("knapsack-graph", "figure"),
+    Input("knapsack-data", "data")
+)
+def update_items_graph(items):
+    # Create / Update the items graph
+    print("Knapsack graph was called")
+    pass
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
